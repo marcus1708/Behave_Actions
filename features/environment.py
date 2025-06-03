53e607a60e15063e1551971bbf5aa97678e2f78e
Marcus Souza <marcusviniciusbordin@gmail.com>
2025-06-03 12:59:40 -0300

    web

--- features/environment.py
+++ features/environment.py
@@ -8,7 +8,6 @@ def before_scenario(context, scenario):
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--user-data-dir=/tmp/chrome_profile_{}".format(id(context)))
         context.driver = webdriver.Chrome(options=chrome_options)
 
 def after_scenario(context, scenario):
