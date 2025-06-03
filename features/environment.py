d3dd676aee21ea3060f0e0a694a191a47e06058a
Marcus Souza <marcusviniciusbordin@gmail.com>
2025-06-03 12:56:03 -0300

    web

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
 def before_scenario(context, scenario):
+  if "web" in scenario.feature.filename.lower():
     context.driver = webdriver.Chrome()
 
 def after_scenario(context, scenario):
